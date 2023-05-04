e9b60d86418a58de6a139f30e93968fa9fe461f5
jungyeonwoo265 <83541644+jungyeonwoo265@users.noreply.github.com>
2023-05-04 11:08:55 +0900

    Add files via upload

---
+++ visual studio/screenSharing_testclient/screenSharing_testclient/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace screenSharing_testclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(695, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "전체 화면 공유";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(535, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 316);
+            this.listBox1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(515, 425);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(535, 355);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 21);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(535, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Server IP";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(537, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 38);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "일부 화면 공유";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ visual studio/screenSharing_testclient/screenSharing_testclient/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//추가
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace screenSharing_testclient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client1;
+        NetworkStream networkstream1;
+        MemoryStream memoryStream1;
+        Bitmap screen1;
+        Thread thread1;
+
+        string serverIP;
+        int serverPort;
+        byte[] data;
+        byte[] dataSizeForServer;
+        int screenmode = 0;
+        int form_X;
+        int form_Y;
+        int form_W;
+        int form_H;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            textBox1.Text = "10.10.21.108";
+        }
+
+        private void ThreadProc()
+        {
+            serverIP = textBox1.Text;
+            serverPort = 9000;
+            try
+            {
+                client1 = new TcpClient(serverIP, serverPort);
+                networkstream1 = client1.GetStream();
+                listBox1.Items.Add("connected to: " + client1.Client.RemoteEndPoint.ToString().Split(':')[0]);
+            }
+            catch(Exception ex) 
+            {
+                listBox1.Items.Add(ex.Message);
+                listBox1.Items.Add(ex.StackTrace);
+
+                return;
+            }
+            try
+            {
+                while(true)
+                {
+                    if(screenmode == 1)
+                        screen1 = GetPartScreen();
+                    else
+                        screen1 = GetAllScreen();
+                    memoryStream1 = new MemoryStream();
+                    screen1.Save(memoryStream1, System.Drawing.Imaging.ImageFormat.Png);
+                    data = memoryStream1.ToArray();
+                    pictureBox1.Image = screen1;
+
+                    if (networkstream1.CanWrite)
+                    {
+                        dataSizeForServer = BitConverter.GetBytes(data.Length);
+                        networkstream1.Write(dataSizeForServer, 0, dataSizeForServer.Length);
+
+                        networkstream1.Write(data, 0, data.Length);
+                        //listBox1.Items.Add("Data sent: " + (data.Length / 1024).ToString() + "Kb");
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+            catch( Exception ex )
+            {
+                listBox1.Items.Add(ex.Message);
+                listBox1.Items.Add(ex.StackTrace);
+            }
+            client1.Close();
+            networkstream1.Close();
+            memoryStream1.Close();
+        }
+
+        private Bitmap GetAllScreen()
+        {
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+            return bitmap;
+        }
+
+        private Bitmap GetPartScreen()
+        {
+            if(this.Top > 0 && this.Left >0)
+            {
+                if (form_X != this.Width)
+                    form_X = this.Width;
+                if (form_Y != this.Height)
+                    form_Y = this.Height;
+                if (form_W != this.Left)
+                    form_W = this.Left;
+                if (form_H != this.Top)
+                    form_H = this.Top;
+            }
+
+            Bitmap bitmap = new Bitmap(form_X,form_Y);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen(form_W, form_H, 0, 0, bitmap.Size);
+
+            return bitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Thread_Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            screenmode = 0;
+            if (thread1 == null || !thread1.IsAlive)
+            {
+                thread1 = new Thread(new ThreadStart(ThreadProc));
+                thread1.Start();
+                listBox1.Items.Add("connecting to the server ...");
+            }
+        }
+
+        private void Thread_Close()
+        {
+            if (thread1 != null && thread1.IsAlive)
+            {
+                thread1.Abort();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            screenmode = 1;
+            if (thread1 == null || !thread1.IsAlive)
+            {
+                thread1 = new Thread(new ThreadStart(ThreadProc));
+                thread1.Start();
+                listBox1.Items.Add("connecting to the server ...");
+            }
+
+        }
+    }
+}
